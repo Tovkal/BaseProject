a49eaa85e1e633f12b4854e95043d33290ceaf9b
Andrés Pizá <Tovkal@gmail.com>
2019-02-23 16:26:43 +0100

    Create project

---
+++ BaseProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaseProject
+//
+//  Created by Andrés Pizá Bückmann on 23/02/2019.
+//  Copyright © 2019 Andrés Pizá Bückmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
